69294792f9c2d34d2206db026163342ea718ea64
Sheikh <tvemperror00@gmail.com>
2023-12-30 00:05:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Splay binary search tree on Python
+A simple fully-working realization of Splay binary search tree as a class on Python programming language.
+_This realization was originally created in march 2022._
+## What is a Splay-tree
+A splay tree is a binary search tree with the additional property that recently accessed elements are quick to access again. Like self-balancing binary search trees, a splay tree performs basic operations such as insertion, look-up and removal in O(log n) amortized time. For random access patterns drawn from a non-uniform random distribution, their amortized time can be faster than logarithmic, proportional to the entropy of the access pattern. For many patterns of non-random operations, also, splay trees can take better than logarithmic time, without requiring advance knowledge of the pattern.
+## Operations in a Splay-tree
+### Splay
+An operation that bring the accesed node of the tree to its root.
+### Zig step (right rotation of the tree)
+A suboperation that rotates a subtree of the Splay-tree right.
+### Zag step (left rotation of the tree)
+A suboperation that rotates a subtree of the Splay-tree left.
+### Insertion
+An operation that inserts a node into the Splay-tree.
+### Deletion
+An operation that deletes a node from the Splay-tree.
+## Methods of the Splay-tree class
+All of the operations described above are realized as methods of the Splay-tree class. In adddition to that, all of the functions of a Python dictionary are among the methods of this class.
---
+++ splay.py
@@ -0,0 +1,116 @@
+def NewNode(key = None, value = None, left = None, right = None):
+  node = [key, value, left, right]
+
+  return node
+
+class Dict:
+  def __init__(self):
+    self.len = 0
+    self.root = None
+  def rightrotate(self, node):
+    left_node = node[2]
+    node[2] = left_node[3]
+    left_node[3] = node
+    return left_node
+  def leftrotate(self, node):
+    right_node = node[3]
+    node[3] = right_node[2]
+    right_node[2] = node
+    return right_node
+  def splay(self, root, key):
+    if root == None or root[0] == key: return root
+    if root[0] > key:
+      if root[2] == None: return root
+      if root[2][0] > key:
+        root[2][2] = self.splay(root[2][2], key)
+        root = self.rightrotate(root)
+      elif root[2][0] < key:
+        root[2][3] = self.splay(root[2][3], key)
+
+        if root[2][3] != None:
+          root[2] = self.leftrotate(root[2])
+      if root[2] == None: return root
+      else: return self.rightrotate(root)
+    else:
+      if root[3] == None: return root
+
+      if root[3][0] > key:
+        root[3][2] = self.splay(root[3][2], key)
+
+        if root[3][2] != None:
+          root[3] = self.rightrotate(root[3])
+      elif root[3][0] < key:
+        root[3][3] = self.splay(root[3][3], key)
+        root = self.leftrotate(root)
+      if root[3] == None: return root
+      else: return self.leftrotate(root)
+
+  def __getitem__(self, key):
+    result = self.root
+    self.root = self.splay(result, key)
+    if self.root[0] == key: return self.root[1]
+    else: raise KeyError(key)
+
+  def insert(self, root, key, value):
+    if root == None: 
+      self.len += 1
+      return NewNode(key, value)
+
+    root = self.splay(root, key)
+    if root[0] == key:
+      root[1] = value
+      return root
+
+    newnode = NewNode(key, value)
+    if root[0] > key:
+      newnode[3] = root
+      newnode[2] = root[2]
+      root[2] = None
+    else:
+      newnode[2] = root
+      newnode[3] = root[3]
+      root[3] = None
+    self.len += 1
+    return newnode
+
+  def __setitem__(self, key, value):
+    self.root = self.insert(self.root, key, value)
+
+  def delete(self, root, key):
+    temp = NewNode()
+    if root == None: return None
+
+    root = self.splay(root, key)
+
+    if key != root[0]: return root
+
+    if root[2] == None:
+      temp = root
+      root = root[3]
+
+    else:
+      temp = root
+
+      root = self.splay(root[2], key)
+
+      root[3] = temp[3]
+
+    return root
+
+  def __delitem__(self, key):
+    result = self.delete(self.root, key)
+    if result == None or result == self.root:
+      raise KeyError(key)
+    else:
+      self.root = result
+      self.len -= 1
+
+  def __contains__(self, key):
+    result = self.root
+    self.root = self.splay(result, key)
+    if self.root == None: return False
+    if self.root[0] == key: return True
+    else: return False
+
+  def __len__(self):
+    return self.len
